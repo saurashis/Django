403660996526d486a0f6bae7c42292b4d3665e57
saurashis <saurashis459@gmail.com>
2024-11-15 08:21:30 +0530

    home, main,about views are added

--- optical_main/views.py
+++ optical_main/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.shortcuts import HttpResponse,redirect
 # Create your views here.
+
+def main(request):
+    return HttpResponse('This main page')
+
+def home(request):
+    return redirect('main')  #This will redirect to main upon typing the /home
+
+def about(request):
+    return HttpResponse('This about page')
+
